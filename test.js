47305205a5cca59320272c708846530ba16ebb2b
Nate <homokanye70@gmail.com>
2025-09-16 10:12:57 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,13 @@
 const assert = require('assert');
-
-// Mock window.ethereum for testing
-global.window = { ethereum: { request: async () => ['mockAccount'] } };
+global.window = { ethereum: { request: async () => ['0x1234...5678'] } };
 const ethers = require('ethers');
-
-// Test connect function (adapt from script.js)
-async function mockConnect() {
-  try {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    assert.strictEqual(accounts[0], 'mockAccount', 'Account mismatch');
-    console.log('Connect test passed');
-  } catch (error) {
-    assert.fail('Connect failed');
-  }
+async function testConnect() {
+    try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        assert.strictEqual(accounts[0], '0x1234...5678', 'Account mismatch');
+        console.log('Connect test passed');
+    } catch (error) {
+        assert.fail('Connect failed');
+    }
 }
-
-mockConnect();
+testConnect();
